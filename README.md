15bc83b504626cb422f12722c7ecd29856bbcbbd
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-12-04 23:27:04 +0900

    [CHORE] 초기세팅

---
+++ README.md
@@ -0,0 +1,3 @@
+# HSKit
+
+A description of this package.
---
+++ Sources/HSKit/HSKit.swift
@@ -0,0 +1,8 @@
+public struct HSKit {
+    public private(set) var text = "Hello, World!"
+    
+    public var message = "축하합니다"
+
+    public init() {
+    }
+}
